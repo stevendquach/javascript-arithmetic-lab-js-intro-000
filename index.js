e3999be7d4cd375a94bdc7a792a5bcf5788ca187
Steven Quach <stevendquach@gmail.com>
2020-08-12 06:51:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function add() {
-  1 + 80 
+  1 + 80 // 81
+}
+function subtract(){
+  60 - 40 // 20
+}
+function multiply() {
+  2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
+}
+function divide() {
+  5.0 / 2.5 // 2
 }
