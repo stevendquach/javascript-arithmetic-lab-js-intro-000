3b79495091675e59ec9da2fcc8e9fb1fa1f71fa8
Steven Quach <stevendquach@gmail.com>
2020-08-12 07:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function subtract(a,b){
   return (a - b) // 20
 }
 function multiply() {
-  2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
+  return (2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
 }
 function divide() {
-  5.0 / 2.5 // 2
+  return (5.0 / 2.5) // 2
 }
