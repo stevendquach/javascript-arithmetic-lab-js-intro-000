16d47f9e39c47d2daf9c39c8c891797dcd21b131
Steven Quach <stevendquach@gmail.com>
2020-08-12 06:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var number = 5
- 
+
 number++ // 5... hmmmm
- 
+
 number // 6 -- the number was incremented after it was evaluated
- 
+
 number-- // 6
- 
+
 number // 5
 --number // 4
- 
+
 ++number // 5
 
 
