e73b8844cacab9d48c78573dee3196464d68dc99
Steven Quach <stevendquach@gmail.com>
2020-08-12 07:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+var a = 1 var b = 80 
 function add(a,b) {
   return (a + b) // 81
 }
 function subtract(a,b){
-  return 60 - 40 // 20
+  return (a - b) // 20
 }
 function multiply() {
   2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
